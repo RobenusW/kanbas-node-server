8472a1f2c3a8bd34858fb305041a3aab16e14978
RobenusW <robenuswork@gmail.com>
2024-12-06 19:12:10 -0500

    a6 server

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-import express from 'express'
+import express from "express";
 import session from "express-session";
-import Hello from "./Hello.js"
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-import cors from 'cors'
+import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from './Kanbas/Assignments/routes.js';
-import enrollmentRoutes from './Kanbas/Enrollments/routes.js';
-
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import enrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+import mongoose from "mongoose";
 import "dotenv/config";
 
-const app = express()
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    })
-   );
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kanbas",
-    resave: false,
-    saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
   };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
-    };
-  }
-  
-  app.use(
-    session(sessionOptions)
-  );
-  
-   
+}
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
-enrollmentRoutes(app)
+enrollmentRoutes(app);
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-Hello(app)
-Lab5(app)
-app.listen(process.env.PORT || 4000)
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
